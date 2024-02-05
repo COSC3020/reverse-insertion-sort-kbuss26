39cb8cc88da9d2750178b5f9c5298339b1f4d9a0
Keifer <144736945+kbuss26@users.noreply.github.com>
2024-02-05 14:45:07 -0700

    Solution 1 with code.js
    Not full completion of exercise

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function insertionSortReverse(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        console.log(array);
+        var compVal = array[i];
+        var j; // To avoid instantiating multiple j iterator variables
+        for (j = i; j < array.length - 1 && array[j+1] < compVal; j++) {
+            console.log("j value: " + array[j] + " and i: " + array[i]);
+            array[j] = array[j+1];
+        }
+        array[j] = compVal;
+    }
     return array;
 }
